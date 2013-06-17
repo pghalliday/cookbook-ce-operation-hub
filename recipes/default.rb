eeb198572ab726372a6c3b7a5a5260bc43a6922a
Peter Halliday <pghalliday@gmail.com>
2013-06-17 19:44:15 +0200

    fixed config

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@ file "#{node[:ce_operation_hub][:destination]}/config.json" do
   content <<-EOH
 {
   "ce-front-end": #{node[:ce_operation_hub][:ce_front_end]},
-  "ce-engine: {
+  "ce-engine": {
     "stream": #{node[:ce_operation_hub][:ce_engine][:stream]},
     "status": #{node[:ce_operation_hub][:ce_engine][:result]}
   }
